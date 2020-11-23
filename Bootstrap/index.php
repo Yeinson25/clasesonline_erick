2c42cd2b9b7f7bba5b62d6c8233d0dd4b2a67c56
ccharaco <jcesarchg9@gmail.com>
2020-11-22 22:22:29 -0400

    index de bootstrap

--- Bootstrap/index.php
+++ Bootstrap/index.php
@@ -1,9 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ejemplo con bootstrap</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
 </head>
 <body>
+	<div class="container">
+  	<!-- Content here -->
+	  	<div class="row">
+	  		<div class="col-4">
+	  			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.		
+	  		</div>	
+	  		<div class="col-4">
+	  			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  		</div>
+	  		<div class="col-4">
+	  			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  		</div>
+	  	</div>
+	</div>
 
+
+
+
+
+
+	<script src="js/jquery.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script type="text/javascript" charset="utf-8">
+		.selector-for-some-widget {
+		  box-sizing: content-box;
+		}
+	</script>
 </body>
 </html>
